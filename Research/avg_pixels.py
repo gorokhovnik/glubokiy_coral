e9b7c2f5557e212b772290ef6e7734b309614c2a
gorokhovnik <nikita.gorokhov@gmail.com>
2019-03-11 16:40:22 +0300

    correct avg pixels, deleted 393762 from train

--- Research/avg_pixels.py
+++ Research/avg_pixels.py
@@ -1,83 +1,88 @@
 from PIL import Image
 import os
 import matplotlib.pyplot as plt
-
+import numpy as np
 import config
 
-train_colors, test_colors, value_test = [], [], []
+train_colors, test_colors, value_colors, total_colors = [], [], [], []
+train_list, test_list, value_list, total_list = [], [], [], []
 
-temp_list = []
-test_dests = [config.TO_TRAIN_DATA + "/" + i for i in os.listdir(config.TO_TRAIN_DATA)]
-for dest in test_dests:
+train_dests = [config.TO_TRAIN_DATA + "/" + i for i in os.listdir(config.TO_TRAIN_DATA)]
+
+for dest in train_dests:
     im = Image.open(dest)
     im_rgb = im.convert('RGB')
+    temp_list = []
     for i in range(50):
         for j in range(50):
             r, g, b = im_rgb.getpixel((i, j))
-            temp_list.append([r, g, b])
+            total_list += [r]
+            train_list += [r]
+            temp_list += [r]
+    train_colors += [np.mean(temp_list)]
+    total_colors += [np.mean(temp_list)]
+train_mean = np.mean(train_colors)
 
-    train_colors += [sum([i[0] for i in temp_list]) / len([i[0] for i in temp_list])]
+plt.title('train_avg')
+plt.hist(train_colors, bins=50, color='red')
+plt.show()
+plt.title('train_pixels')
+plt.hist(train_list, bins=50, color='orange')
+plt.show()
+print('train mean: ', train_mean)
 
-temp_list = []
-test_dests = [config.TO_TEST_DATA + "/" + i for i in os.listdir(config.TO_TEST_DATA)]
-for dest in test_dests:
-    im = Image.open(dest)
-    im_rgb = im.convert('RGB')
-
-    for i in range(50):
-        for j in range(50):
-            r, g, b = im_rgb.getpixel((i, j))
-            temp_list.append([r, g, b])
-
-    test_colors += [sum([i[0] for i in temp_list]) / len([i[0] for i in temp_list])]
-#
 val_dests = [config.TO_VALUE_DATA + "/" + i for i in os.listdir(config.TO_VALUE_DATA)]
-temp_list = []
+
 for dest in val_dests:
     im = Image.open(dest)
     im_rgb = im.convert('RGB')
-
+    temp_list = []
     for i in range(50):
         for j in range(50):
             r, g, b = im_rgb.getpixel((i, j))
-            temp_list.append([r, g, b])
+            total_list += [r]
+            value_list += [r]
+            temp_list += [r]
+    value_colors += [np.mean(temp_list)]
+    total_colors += [np.mean(temp_list)]
+value_mean = np.mean(value_colors)
 
-    value_test += [sum([i[0] for i in temp_list]) / len([i[0] for i in temp_list])]
-# plt.hist(train_clors)
-# plt.hist(test_colors)
-colors = ['Red', 'Yellow', 'Green']
-names = ['value', 'test', 'train']
-for nonuse, j in enumerate([value_test]):
-    plt.title(names[nonuse])
-
-    plt.hist([int(i) for i in j], bins=50, color=colors[nonuse])
-    plt.show()
-    # plt.hist([i[1] for i in j], bins=50)
-    # plt.show()
-    # plt.hist([i[2] for i in j], bins=50)
-    # plt.show()
-
-plt.title('Total')
-plt.hist([int(i) for i in value_test + test_colors + train_colors], bins=50, color='Purple')
+plt.title('value_avg')
+plt.hist(value_colors, bins=50, color='yellow')
 plt.show()
-# plt.hist([i[1] for i in value_test+test_colors+train_clors], bins=50)
-# plt.show()
-# plt.hist([i[2] for i in value_test+test_colors+train_clors], bins=50)
-# plt.show()
+plt.title('value_pixels')
+plt.hist(value_list, bins=50, color='green')
+plt.show()
+print('value mean: ', value_mean)
 
+test_dests = [config.TO_TEST_DATA + "/" + i for i in os.listdir(config.TO_TEST_DATA)]
 
-names = ['train', 'value', 'test']
-for i, j in enumerate([train_colors, value_test, test_colors]):
-    print("Start avg %s" % names[i])
-    print(sum([i[0] for i in j]) / len([i[0] for i in j]))
-    print(sum([i[1] for i in j]) / len([i[1] for i in j]))
-    print(sum([i[2] for i in j]) / len([i[2] for i in j]))
-    print("End \n\n")
+for dest in test_dests:
+    im = Image.open(dest)
+    im_rgb = im.convert('RGB')
+    temp_list = []
+    for i in range(50):
+        for j in range(50):
+            r, g, b = im_rgb.getpixel((i, j))
+            total_list += [r]
+            test_list += [r]
+            temp_list += [r]
+    test_colors += [np.mean(temp_list)]
+    total_colors += [np.mean(temp_list)]
+test_mean = np.mean(test_colors)
 
-print("All avgs")
-print(sum([i[0] for i in value_test + test_colors + train_colors]) / len(
-    [i[0] for i in value_test + test_colors + train_colors]))
-print(sum([i[1] for i in value_test + test_colors + train_colors]) / len(
-    [i[1] for i in value_test + test_colors + train_colors]))
-print(sum([i[2] for i in value_test + test_colors + train_colors]) / len(
-    [i[2] for i in value_test + test_colors + train_colors]))
+plt.title('test_avg')
+plt.hist(test_colors, bins=50, color='blue')
+plt.show()
+plt.title('test_pixels')
+plt.hist(test_list, bins=50, color='purple')
+plt.show()
+print('test mean: ', test_mean)
+
+plt.title('total_avg')
+plt.hist(total_colors, bins=50, color='cyan')
+plt.show()
+plt.title('total_pixels')
+plt.hist(total_list, bins=50, color='pink')
+plt.show()
+print('total mean: ', np.mean(total_colors))
