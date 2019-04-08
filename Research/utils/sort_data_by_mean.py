ac502ea4ed87b3c592b086f2fc73f784c1f4735c
CheshireB <nikitauniversity@yandex.ru>
2019-04-08 20:03:20 +0300

    Class factory. Sorting images

--- Research/utils/sort_data_by_mean.py
+++ Research/utils/sort_data_by_mean.py
@@ -6,7 +6,9 @@ from Research.analog_avg_pixels import ImageInfo
 
 dests_dict = {
     'train': {
+        # List of absolute destination to pictures
         'dests': ['{}/{}'.format(config.TO_TRAIN_DATA, name) for name in os.listdir(config.TO_TRAIN_DATA)],
+        # Mean filter
         'mean': 69.15410413030831,
     },
     'value': {
@@ -21,17 +23,23 @@ dests_dict = {
 
 dest_to_target = config.TO_ROOT + '/Research/data_prepared/sort_by_mean'
 
+# Iter tor train, value and test data
 for key, value in dests_dict.items():
     count = len(value['dests'])
+    # Iter in every dest in train, value, test data
     for i, dest in enumerate(value['dests']):
         img = ImageInfo(dest)
 
+        # Get name of file which will copy
         name = dest.split('/')[-1]
+        # Get mean and choose the folder (greater or less)
         mean_type = 'higher' if img.mean < value['mean'] else 'lower'
-
+        # Process new destination
         new_dest = '{}/{}/{}/{}'.format(dest_to_target, mean_type, key, name)
+        # Copying
         copyfile(dest, new_dest)
 
+        # Lof of work
         print('[{}/{}] {} {} -> {}'.format(
             i, count, mean_type.capitalize().ljust(6, ' '), dest.ljust(95, ' '), new_dest))
 
